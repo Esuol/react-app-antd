193331fc6206c749db33d8f5b79ccf198ee2c1ab
weibin <1292393108@qq.com>
2019-04-19 15:07:22 +0800

    initProject

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import './index.css';
-import App from './pages/App/App';
+import App from './layouts/index';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/layouts/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SideMenu from './sideMenu';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends React.Component {
+  render() {
+    return <SideMenu />;
+  }
+}
