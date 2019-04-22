7193d76017909456ff8e772ad3d508ef3a76242f
weibin <1292393108@qq.com>
2019-04-22 14:28:59 +0800

    updateStyle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-import './index.css';
+import './global.less';
 import App from './layouts/index';
 import * as serviceWorker from './serviceWorker';
 
