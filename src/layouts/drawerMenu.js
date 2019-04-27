076ea0b769bee0bdc7b4fdcc1c0d1b41653d388c
weibin <1292393108@qq.com>
2019-04-27 23:57:40 +0800

    drawer

---
+++ src/layouts/drawerMenu.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Drawer, Icon } from 'antd'
+import styles from './index.less'
+
+// eslint-disable-next-line react/prefer-stateless-function
+class DrawerMenu extends React.Component {
+  constructor (props) {
+    super(props)
+    console.log(props)
+  }
+
+  render () {
+    const { drawerVisible, closeDrawer } = this.props
+
+    return (
+
+
+        <Drawer
+          title="Basic Drawer"
+          placement='left'
+          closable={false}
+          onClose={ () => closeDrawer() }
+          visible={drawerVisible}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+           <Icon type="close" className={styles.drawerClose} onClick={() => closeDrawer() } />
+        </Drawer>
+
+
+    )
+  }
+
+}
+
+export default DrawerMenu
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,33 +1,59 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import { Layout } from 'antd';
-
+import { Layout, Icon } from 'antd';
 import SideMenu from './sideMenu'
 import HeaderView from './header'
 import Content from './content'
-
-import './index.less';
-
-
+import DrawerMenu from './drawerMenu'
+import styles from './index.less';
 
 class SiderDemo extends React.Component {
   constructor (props) {
     super(props)
     this.setParentState = this.toggle.bind(this);
+    this.closeDrawer = this.closeDrawerPar.bind(this)
   }
 
   state = {
-    collapsed: false
+    collapsed: false,
+    currentWidth: document.body.clientWidth,
+    drawerVisible: false
   };
+
+  componentDidMount () {
+    window.addEventListener('resize', this.resizeBind)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.resizeBind)
+  }
+
+  resizeBind = () => {
+    this.setState(() => ({ currentWidth: document.body.clientWidth }))
+  }
 
   toggle = () => {
     this.setState(prevState => ({ collapsed: !prevState.collapsed }));
   };
 
+  showDrawerMenu = () => {
+   this.setState( () => ({ drawerVisible: true}))
+  }
+
+  closeDrawerPar = () => {
+    this.setState( prevState => ({ drawerVisible: !prevState.drawerVisible}))
+  }
+
   render() {
-    const { collapsed } = this.state;
+    const { collapsed, currentWidth, drawerVisible } = this.state;
     return (
       <Layout>
-        <SideMenu trigger={null} collapsed={collapsed} />
+        {currentWidth < 768
+        ? <div className={styles.iconWrap} onClick={this.showDrawerMenu} role="presentation">
+            <Icon type="bars" style={{fontSize: '20px',width: '100%', textAlign: 'center', lineHeight: '30px'}} />
+            <DrawerMenu drawerVisible={drawerVisible} closeDrawer={this.closeDrawer} />
+          </div>
+        : <SideMenu trigger={null} collapsed={collapsed} />}
         <Layout>
           <HeaderView collapsed={collapsed} setParentState={this.setParentState} />
           <Content />
