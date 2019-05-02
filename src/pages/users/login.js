c9b9fb7c168046c64a8f3970007f580b9dbc1df3
weibin <1292393108@qq.com>
2019-05-02 19:00:46 +0800

    loginPAge

--- src/pages/users/login.js
+++ src/pages/users/login.js
@@ -1,9 +1,61 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react'
+import Particles from 'react-particles-js';
+import './index.less'
 
-const Login = React.memo( () => {
+class Login extends React.Component {
+
+  render () {
     return (
-      <p>login</p>
+      <div className="login">
+        <Particles
+              params={{
+                "particles": {
+                    "number": {
+                        "value": 500,
+                        "density": {
+                            "enable": true,
+                            "value_area": 1500
+                        }
+                    },
+                    "line_linked": {
+                        "enable": true,
+                        "opacity": 0.02
+                    },
+                    "move": {
+                        "direction": "right",
+                        "speed": 0.05
+                    },
+                    "size": {
+                        "value": 1
+                    },
+                    "opacity": {
+                        "anim": {
+                            "enable": true,
+                            "speed": 1,
+                            "opacity_min": 0.05
+                        }
+                    }
+                },
+                "interactivity": {
+                    "events": {
+                        "onclick": {
+                            "enable": true,
+                            "mode": "push"
+                        }
+                    },
+                    "modes": {
+                        "push": {
+                            "particles_nb": 1
+                        }
+                    }
+                },
+                "retina_detect": true}}
+              className="login"
+              />
+      </div>
     )
-})
+  }
+}
 
 export default Login
