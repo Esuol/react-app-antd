83c0a18447aa4f4d34f9d38167e37d120a21c452
weibin <1292393108@qq.com>
2019-06-12 19:02:38 +0800

    update

--- src/pages/dashboard/workbench.js
+++ src/pages/dashboard/workbench.js
@@ -7,7 +7,7 @@ class WorkBentch extends React.Component {
   render () {
     const pageHeaderContent = (
       <div>
-        <p>早安，Berlin，祝你开心工作每一天！</p>
+        <p className="f20px title">早安，Berlin，祝你开心工作每一天！</p>
         <p>陌生人，我也为你祝福 愿你有一个灿烂的前程 愿你有情人终成眷属 愿你在尘世获的幸福</p>
       </div>
     )
