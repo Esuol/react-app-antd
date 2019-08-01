850e45e46a439247a66069f2ca1fd78498b581f8
weibin <1292393108@qq.com>
2019-08-01 15:07:17 +0800

    修复路由key重复bug

--- src/router/form.js
+++ src/router/form.js
@@ -1,43 +1,43 @@
-import React from 'react'
-import Loadable from 'react-loadable'
-import Loading from '../components/jumpLoading/jumpLoading'
+import React from 'react';
+import Loadable from 'react-loadable';
+import Loading from '../components/jumpLoading/jumpLoading';
 
-export default{
+export default {
   key: 'group2',
   title: {
-      icon: 'ordered-list',
-      text: '表单页'
+    icon: 'ordered-list',
+    text: '表单页'
   },
   children: [
-      {
-          key: '3',
-          text: '基础表单',
-          path: '/form/baseForm',
-          exact: true,
-          component: Loadable({
-            loader: () => import('../pages/form/baseForm'),
-            loading: () => <Loading state />
-          })
-      },
-      {
-          key: '4',
-          text: '分步表单',
-          path: '/form/stepForm',
-          exact: true,
-          component: Loadable({
-            loader: () => import('../pages/form/stepForm'),
-            loading: () => <Loading state />
-          })
-      },
-      {
-          key: '5',
-          text: '高级表单',
-          path: '/form/advancedForm',
-          exact: true,
-          component: Loadable({
-            loader: () => import('../pages/form/advancedForm'),
-            loading: () => <Loading state />
-          })
-      }
+    {
+      key: '4',
+      text: '基础表单',
+      path: '/form/baseForm',
+      exact: true,
+      component: Loadable({
+        loader: () => import('../pages/form/baseForm'),
+        loading: () => <Loading state />
+      })
+    },
+    {
+      key: '5',
+      text: '分步表单',
+      path: '/form/stepForm',
+      exact: true,
+      component: Loadable({
+        loader: () => import('../pages/form/stepForm'),
+        loading: () => <Loading state />
+      })
+    },
+    {
+      key: '6',
+      text: '高级表单',
+      path: '/form/advancedForm',
+      exact: true,
+      component: Loadable({
+        loader: () => import('../pages/form/advancedForm'),
+        loading: () => <Loading state />
+      })
+    }
   ]
-}
+};
