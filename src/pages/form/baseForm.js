ed3344d85178d8d107fd0c30088f26921b5d13b1
weibin <1292393108@qq.com>
2019-08-02 10:34:58 +0800

    update

--- src/pages/form/baseForm.js
+++ src/pages/form/baseForm.js
@@ -1,10 +1,53 @@
-import React from 'react'
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/require-render-return */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { connet } from 'react-redux';
+import {
+  Form,
+  Input,
+  DatePicker,
+  Select,
+  Button,
+  Card,
+  InputNumber,
+  Radio,
+  Icon,
+  Tooltip
+} from 'antd';
+import './index.less';
 
-// eslint-disable-next-line no-unused-vars
-const BaseForm = React.memo(props => {
-  return (
-    <p>基础表单</p>
-  )
-})
+const FormItem = Form.Item;
+const { Option } = Select;
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
 
-export default BaseForm
+class BaseForm extends React.Component {
+  render() {
+    const { submitting } = this.props;
+    const {
+      form: { getFieldDecorator, getFieldValue }
+    } = this.props;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 7 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 12 },
+        md: { span: 10 }
+      }
+    };
+
+    const submitFormLayout = {
+      wrapperCol: {
+        xs: { span: 24, offset: 0 },
+        sm: { span: 10, offset: 7 }
+      }
+    };
+  }
+}
+
+export default BaseForm;
