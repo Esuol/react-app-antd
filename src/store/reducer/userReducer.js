9eee1cb4666df6b5e4b7c0ad195b2dc8f9a2d4c0
berlinen <1292393108@qq.com>
2019-08-21 14:43:35 +0800

    update

--- src/store/reducer/userReducer.js
+++ src/store/reducer/userReducer.js
@@ -1,26 +1,27 @@
 /* eslint-disable consistent-return */
-import { userAction } from '../actions'
-import initalState from '../state'
+// reduer 应用
+import { userAction } from '../actions';
+import initalState from '../state';
 
-export default function (state = initalState.userState, action) {
-  switch(action.type) {
-     case userAction.FETCH_LOGIN_BEGIN:
+export default function(state = initalState.userState, action) {
+  switch (action.type) {
+    case userAction.FETCH_LOGIN_BEGIN:
       return {
         ...state,
-        loading: true,
+        loading: true
       };
-      case userAction.FETCH_LOGIN_SUCCESS:
-        return Object.assign({}, state, {
-          loading: false,
-          token: action.data.token,
-          nickName: action.data.nickName
-        })
-      case userAction.FETCH_LOGIN_FAILURE:
-        return {
-          ...state,
-          loading: false
-        }
-      default:
-        return state;
+    case userAction.FETCH_LOGIN_SUCCESS:
+      return Object.assign({}, state, {
+        loading: false,
+        token: action.data.token,
+        nickName: action.data.nickName
+      });
+    case userAction.FETCH_LOGIN_FAILURE:
+      return {
+        ...state,
+        loading: false
+      };
+    default:
+      return state;
   }
 }
